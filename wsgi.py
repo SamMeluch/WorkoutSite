72a9665fb10ebc6f383e822afde3196ed65a74f1
root <root@frostbite.server>
2019-09-21 19:20:52 -0500

    initial setup stuff

--- wsgi.py
+++ wsgi.py
@@ -7,15 +7,20 @@ class Resource(object):
     def on_get(self, req, resp):
         doc = {
             'images': [{
-                'href': '216.png'
+                'href': 'fractals/216.png'
              }]
         }
 
-        resp.body = json.dumps(doc, ensure_ascii=False)
-
+        print('got inside function')
+        resp.body = json.dumps(doc)
+        resp.content_type = 'text/json'
         resp.status = falcon.HTTP_200
 
-api = application = falcon.API()
+api = falcon.API()
 
 images = Resource()
-api.add_route('/images', images)
+api.add_route('/api/img', images)
+application = api
+#def application(env, start_response):
+#   start_response('200 OK', [('Content-Type', 'text/html')]
+#   return [b"Hello World"]
